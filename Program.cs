9cf94dcf0929bc71fea879101fb6fc26c548b8d1
Alexander <mypost@example.com>
2023-01-21 19:53:47 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43. Найти точку пересечения двух прямых
+Console.Clear();
+
+Console.WriteLine("Задайте точку b1");
+decimal b1 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Задайте точку k1");
+decimal k1 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Задайте точку b2");
+decimal b2 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Задайте точку k2");
+decimal k2 = Convert.ToDecimal(Console.ReadLine());
+
+if (b1 == b2 && k1 == k2){
+    Console.WriteLine("Эти линии параллельны. Они имеют бесконечное множество точек.");
+} else {
+    decimal x = (b2 - b1) / (k1 - k2);
+    decimal y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения {x}; {y}");
+}
